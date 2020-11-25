f7319f41fdc03086c823e16d23c040cc3b3d088a
LuisDHR <ldwhr01@gmail.com>
2020-11-25 00:18:23 -0600

    Perder vida

--- src/scenes/ElementosHUD.js
+++ src/scenes/ElementosHUD.js
@@ -17,14 +17,27 @@ class ElementosHUD extends Phaser.Scene {
             console.log('se suma una vida', this.numero_vidas);
         });
 
-        this.registry.events.on('vida_resta', dato => {
-            this.numero_vidas -= 1;
-            console.log('se suma una vida', this.numero_vidas);
+        this.registry.events.on('vida_resta', () => {
+            // this.numero_vidas -= 1;
+            this.perderVida();
+            console.log('se resta una vida', this.numero_vidas);
         });
     }
 
     create() {
         this.corazon = this.add.image(50, 50, 'corazon').setScale(2.5);
+        this.textoVidas = this.add.text(44, 38, `${this.numero_vidas}`, { font: '20px Arial' });
+        this.textoVidas.setTint(0x000000);
+    }
+
+    perderVida() {
+        if ( this.numero_vidas > 1 ) {
+            this.numero_vidas -= 1;
+            this.textoVidas.setText(`${this.numero_vidas}`);
+        }
+        else {
+            // Game over
+        }
     }
 
     update(time, delta) {
